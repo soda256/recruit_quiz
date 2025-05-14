25f26b734832eb25909a66f9001ec19c13f01360
soda256 <itsuki2828ki@gmail.com>
2025-05-14 15:27:01 +0900

    うわあああああああああああ

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -81,6 +81,16 @@ int main()
 	}
 	question.push_back({ to_string(x) + "人のうち" + to_string(y) + "人を選んで並べる方法は何通りあるか？", to_string(z) });
 
+	//組み合わせ
+	x = uniform_int_distribution<>(3, 6)(rand);
+	y = uniform_int_distribution<>(1, x)(rand);
+	z = 1;
+	for (int i = 0; i < y; i++)
+	{
+		z /= y - i;
+	}
+	question.push_back({ to_string(x) + "人のうち" + to_string(y) + "人を選ぶ組み合わせは何通りあるか？", to_string(z) });
+
 	std::cout << "[リクルート試験対策クイズ]\n";
 
 	for (const auto& e : question)
