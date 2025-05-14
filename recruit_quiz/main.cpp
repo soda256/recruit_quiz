38c9a87eeaa93bdc601ba9b63875fdcba03f8166
soda256 <itsuki2828ki@gmail.com>
2025-05-14 14:53:00 +0900

    円錐の問題を追加

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -38,10 +38,15 @@ int main()
 		to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")÷" + to_string(w) + "の答えは？";
 	question[2].a = x - (y + z);
 
-	//三角形の面積
+	//三角形の高さ
 	x = uniform_int_distribution<>(1, 10)(rand);
 	y = uniform_int_distribution<>(1, 5)(rand) * 2;
 	question.push_back({ "面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ。", x });
+
+	//円錐の体積
+	x = uniform_int_distribution<>(1, 10)(rand);
+	y = uniform_int_distribution<>(1, 5)(rand) * 3;
+	question.push_back({ "底面の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n" + "この円錐の体積をXπcm^3とする。Xの値を求めよ。", x * x * y / 3 });
 
 	cout << "[リクルート試験対策クイズ]\n";
 
