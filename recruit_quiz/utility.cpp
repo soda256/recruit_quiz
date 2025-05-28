7a9df30b5f997c244092ef3a1e300417e5c5a659
soda256 <itsuki2828ki@gmail.com>
2025-05-28 15:24:57 +0900

    意味から熟語を選ぶ問題を追加

--- recruit_quiz/utility.cpp
+++ recruit_quiz/utility.cpp
@@ -14,6 +14,16 @@ std::vector<int> CreateRandomIndices(int n)
     }
 
     //番号の配列をシャッフル
+    Shuffle(indices);
+
+    return indices;
+}
+
+//配列をシャッフルする
+void Shuffle(std::vector<int>& indices)
+{
+    const int n = static_cast<int>(indices.size());
+
     random_device rd;
     mt19937 rand(rd());
     for (int i = n - 1; i > 0; i--)
@@ -23,6 +33,25 @@ std::vector<int> CreateRandomIndices(int n)
         indices[i] = indices[j];
         indices[j] = tmp;
     }
+}
+
+//間違った番号の配列を作成する
+std::vector<int> CreateWrongIndices(int n, int correctIndex)
+{
+    //番号を配列に格納
+    vector<int> indices(n - 1);
+
+    for (int i = 0; i < correctIndex; i++)
+    {
+        indices[i] = i;
+    }
+    for (int i = correctIndex; i < n - 1; i++)
+    {
+        indices[i] = i + 1;
+    }
+
+    //番号の配列をシャッフル
+    Shuffle(indices);
 
     return indices;
 }
--- recruit_quiz/utility.h
+++ recruit_quiz/utility.h
@@ -3,3 +3,11 @@
 
 //ランダムな配列を作成する
 std::vector<int> CreateRandomIndices(int n);
+
+//配列をシャッフルする
+void Shuffle(std::vector<int>& indices);
+
+//間違った番号の配列を作成する
+//n				作成する番号の範囲
+//correcrIndex	正解の番号
+std::vector<int> CreateWrongIndices(int n, int correctIndex);
