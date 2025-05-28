4b3ed2982914b6195b274b2ed3279fbd864a6261
soda256 <itsuki2828ki@gmail.com>
2025-05-28 15:37:33 +0900

    慣用句の意味をこたえる問題を追加

--- recruit_quiz/exam_japanese.h
+++ recruit_quiz/exam_japanese.h
@@ -2,4 +2,8 @@
 #include "question.h"
 
 
+//Š¿š‚Ì“Ç‚İæ‚è–â‘è‚ğì¬‚·‚é
 QuestionList CreateKanjiExam();
+
+//Šµ—p‹å‚ÌˆÓ–¡‚ğ‚±‚½‚¦‚é–â‘è‚ğì¬‚·‚é
+QuestionList CreateIdiomExam();
--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -27,70 +27,73 @@ int main()
 	random_device rd;
 	mt19937 rand(rd());
 
-	//掛け算
-	int x = uniform_int_distribution<>(1, 30)(rand);
-	int y = uniform_int_distribution<>(1, 20)(rand);
-	question[0].q = to_string(x) + "×" + to_string(y) + "の答えは？";
-	question[0].a = to_string(x * y);
-
-	//割り算
-	x = uniform_int_distribution<>(1, 30)(rand);
-	y = uniform_int_distribution<>(1, 20)(rand);
-	question[1].q = to_string(x * y) + "÷" + to_string(y) + "の答えは？";
-	question[1].a = to_string(x);
-
-	//複雑な式
-	x = uniform_int_distribution<>(1, 100)(rand);
-	y = uniform_int_distribution<>(1, 10)(rand);
-	int z = uniform_int_distribution<>(1, 10)(rand);
-	int w = uniform_int_distribution<>(1, 10)(rand);
-	question[2].q =
-		to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")÷" + to_string(w) + "の答えは？";
-	question[2].a = to_string(x - (y + z));
-
-	//三角形の高さ
-	x = uniform_int_distribution<>(1, 10)(rand);
-	y = uniform_int_distribution<>(1, 5)(rand) * 2;
-	question.push_back({ "面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ。", to_string(x) });
-
-	//円錐の体積
-	x = uniform_int_distribution<>(1, 10)(rand);
-	y = uniform_int_distribution<>(1, 5)(rand) * 3;
-	question.push_back({ "底面の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n" + "この円錐の体積をXπcm^3とする。Xの値を求めよ。", to_string(x * x * y / 3) });
-
-	//球の体積
-	x = uniform_int_distribution<>(1, 5)(rand) * 3;
-	question.push_back({ "球の半径" + to_string(x) + "cmの球がある。\n" + "この球の体積をXπcm^3とする。Xの値を求めよ。", to_string(x * x * x * 4 / 3) });
-
-	//サイコロの確立
-	x = uniform_int_distribution<>(1, 5)(rand);
-	y = uniform_int_distribution<>(1, 6 - x)(rand);
-	z = gcd(y + 1, 6);
-	question.push_back({ "サイコロを1個振って" + to_string(x) + "から" + to_string(x + y) + "が出る確率を求めよ。", to_string((y + 1) / z) + "/" + to_string(6 / z) });
-
-	//順列
-	x = uniform_int_distribution<>(3, 7)(rand);
-	y = uniform_int_distribution<>(1, x)(rand);
-	z = 1;
-	for (int i = 0; i < y; i++)
+	//数学問題
 	{
-		z *= x - i;
-	}
-	question.push_back({ to_string(x) + "人のうち" + to_string(y) + "人を選んで並べる方法は何通りあるか？", to_string(z) });
+		//掛け算
+		int x = uniform_int_distribution<>(1, 30)(rand);
+		int y = uniform_int_distribution<>(1, 20)(rand);
+		question[0].q = to_string(x) + "×" + to_string(y) + "の答えは？";
+		question[0].a = to_string(x * y);
 
-	//組み合わせ
-	x = uniform_int_distribution<>(3, 6)(rand);
-	y = uniform_int_distribution<>(1, x)(rand);
-	z = 1;
-	for (int i = 0; i < y; i++)
-	{
-		z *= x - i;
+		//割り算
+		x = uniform_int_distribution<>(1, 30)(rand);
+		y = uniform_int_distribution<>(1, 20)(rand);
+		question[1].q = to_string(x * y) + "÷" + to_string(y) + "の答えは？";
+		question[1].a = to_string(x);
+
+		//複雑な式
+		x = uniform_int_distribution<>(1, 100)(rand);
+		y = uniform_int_distribution<>(1, 10)(rand);
+		int z = uniform_int_distribution<>(1, 10)(rand);
+		int w = uniform_int_distribution<>(1, 10)(rand);
+		question[2].q =
+			to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")÷" + to_string(w) + "の答えは？";
+		question[2].a = to_string(x - (y + z));
+
+		//三角形の高さ
+		x = uniform_int_distribution<>(1, 10)(rand);
+		y = uniform_int_distribution<>(1, 5)(rand) * 2;
+		question.push_back({ "面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ。", to_string(x) });
+
+		//円錐の体積
+		x = uniform_int_distribution<>(1, 10)(rand);
+		y = uniform_int_distribution<>(1, 5)(rand) * 3;
+		question.push_back({ "底面の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n" + "この円錐の体積をXπcm^3とする。Xの値を求めよ。", to_string(x * x * y / 3) });
+
+		//球の体積
+		x = uniform_int_distribution<>(1, 5)(rand) * 3;
+		question.push_back({ "球の半径" + to_string(x) + "cmの球がある。\n" + "この球の体積をXπcm^3とする。Xの値を求めよ。", to_string(x * x * x * 4 / 3) });
+
+		//サイコロの確立
+		x = uniform_int_distribution<>(1, 5)(rand);
+		y = uniform_int_distribution<>(1, 6 - x)(rand);
+		z = gcd(y + 1, 6);
+		question.push_back({ "サイコロを1個振って" + to_string(x) + "から" + to_string(x + y) + "が出る確率を求めよ。", to_string((y + 1) / z) + "/" + to_string(6 / z) });
+
+		//順列
+		x = uniform_int_distribution<>(3, 7)(rand);
+		y = uniform_int_distribution<>(1, x)(rand);
+		z = 1;
+		for (int i = 0; i < y; i++)
+		{
+			z *= x - i;
+		}
+		question.push_back({ to_string(x) + "人のうち" + to_string(y) + "人を選んで並べる方法は何通りあるか？", to_string(z) });
+
+		//組み合わせ
+		x = uniform_int_distribution<>(3, 6)(rand);
+		y = uniform_int_distribution<>(1, x)(rand);
+		z = 1;
+		for (int i = 0; i < y; i++)
+		{
+			z *= x - i;
+		}
+		for (int i = 0; i < y; i++)
+		{
+			z /= y - i;
+		}
+		question.push_back({ to_string(x) + "人のうち" + to_string(y) + "人を選ぶ組み合わせは何通りあるか？", to_string(z) });
 	}
-	for (int i = 0; i < y; i++)
-	{
-		z /= y - i;
-	}
-	question.push_back({ to_string(x) + "人のうち" + to_string(y) + "人を選ぶ組み合わせは何通りあるか？", to_string(z) });
 
 	std::cout << "[リクルート試験対策クイズ]\n";
 
@@ -100,6 +103,8 @@ int main()
 	if (subject == 2)
 	{
 		question = CreateKanjiExam();
+		const QuestionList idiomExam = CreateIdiomExam();
+		question.insert(question.end(), idiomExam.begin(), idiomExam.end());
 	}
 
 	for (const auto& e : question)
